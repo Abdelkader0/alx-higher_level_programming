b7cb0333668a6f3830e100892ff916a43fc14dcb
Abdelkader0 <ahmedbob43@gmail.com>
2023-06-06 15:23:38 -0700

    a - z

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 # Author - Abdelkader
-for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end=' ')
-
-print()
+for i in range(97, 123):
+    print("{}".format(chr(i)), end='')
